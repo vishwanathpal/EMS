9e705a32dc4623aa6dcaa6c753cd431478dca69c
Vishwanath <vishwanath.pal.hs@gmail.com>
2019-05-24 18:06:21 +0530

    registration form

--- form-security/src/main/java/com/spring/security/controller/MvcConfig.java
+++ form-security/src/main/java/com/spring/security/controller/MvcConfig.java
@@ -1,18 +1,13 @@
 package com.spring.security.controller;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class MvcConfig implements WebMvcConfigurer{
 	
-
+	
 	public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/home").setViewName("home");
         registry.addViewController("/").setViewName("home");
@@ -20,14 +15,26 @@ public class MvcConfig implements WebMvcConfigurer{
         registry.addViewController("/login").setViewName("login");
         registry.addViewController("/user").setViewName("user");
         registry.addViewController("/registration_User_admin").setViewName("registration_User_admin");
+//        registry.addViewController("/registration_User_admin");
     }
 	
-	//Employee login form
-    @RequestMapping(value = "/registration_User_admin")
-    public ModelAndView employeeLoginForm(HttpServletRequest request, HttpServletResponse response) throws Exception 
-    {
-        ModelAndView registration = new ModelAndView("registration_User_admin");
-        return registration;     
+	/*@PostMapping("/registration_User_admin")
+    public String greetingSubmit(@ModelAttribute employee emp) {
+        return "home";
     }
-
+	
+	@RequestMapping(value = "/registration_User_admin", method = RequestMethod.GET)
+    public String showRegistrationForm(WebRequest request, Model model) {
+		employee emp = new employee();
+        model.addAttribute("employee", emp);
+        return "registration_User_admin";
+    } 
+	
+	@RequestMapping("/home")
+	public ModelAndView firstPage() {
+		return new ModelAndView("home");
+	}*/
+	
+	
+	
 }
